2c8c2b476051e8bc0dc7af5382f025ed37c29e14
pc-sam <samurai.h@yahoo.com>
2023-12-13 09:56:12 +0800

    pass all tests

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,19 @@
 // Code EyesOnMe Component Here
+
+function EyesOnMe(){
+    function handlefocus(event){
+        event.preventDefault()
+        console.log("Good!")
+    }
+    
+    function handleBlur(event){
+        event.preventDefault()
+        console.log("Hey! Eyes on me!")
+    }
+    return(
+        <button onFocus={handlefocus} onBlur={handleBlur}>Eyes on me</button>
+    )
+
+}
+
+export default EyesOnMe;
